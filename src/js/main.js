d683b4711b954367b02b894b77bb50bc3603ea1c
Your Name <you@example.com>
2017-08-06 15:21:19 -0400

    Finished project.

--- src/js/main.js
+++ src/js/main.js
@@ -1,133 +1,219 @@
-console.log("I am here!");
-var sS = document.getElementById('schoolField');
-var sN = document.getElementById('nameField');
-var sG = document.getElementById('gradesField');
-var sC = document.getElementById('computerField');
-
-var students = new Array;
-// Setting Item to local Storage.
-localStorage.setItem("students", JSON.stringify(students));
-
+// Creating a Person class
 class Person {
-  constructor(n, g, c) {
-    this.name = n;
-    this.grades = g;
-    this.computer = c;
+  constructor(_name) {
+    this.name = _name;
+  }
+
+  // polymorphism. Same function names are overridden in Employee class
+
+  // Method to display different title string if the position title equals boss.
+  getTitleString() {
+    return "Get back to work!";
+  }
+  // Method to display different hour string if the position title equals boss.
+  getHoursString () {
+    return "Whenever I want!";
   }
 }
 
-Person.school = "Fullsail";
-
-/*
-(function(){
-  for (var i = 0; i < 10; i++) {
-    var student = new Person("max"+i, 15 * i, "mac");
-    students.push(student);
+// Creating an Employee class and inheriting from Person object.
+class Employee extends Person {
+  constructor(_name, _position, _parkingSpotNumber) {
+    super(_name);
+    this.parkingSpotNumber = _parkingSpotNumber;
+    this.position = _position;
   }
 
-  Person.school = "UGB"
-
-  var grades = [70, 80, 90];
-  console.log(Utils.getAverage(grades));
-
-  displayInfo();
-
-  function displayInfo() {
-    students.forEach(function(e){
-      // This is where I need to print values to the Table.
-      console.log("Student Name: " + e.name + "\nStudent Grade: " + e.grades + "\nStudent Computer: " + e.computer + "\nSchool: " + Person.school);
-    })
+  // Method to display different title string if the position title equals anything except boss.
+  getTitleString() {
+    return this.position.title;
   }
-
-})();
-*/
-
-function populateSchoolField() {
-  console.log("Loaded!");
-  sS.value = Person.school;
+  // Method to display different hour string if the position title equals anything except boss.
+  getHoursString() {
+    // make sure the hour string is right
+    return this.position.hours;
+  }
 }
-window.onload = populateSchoolField;
 
-// onClick event for Add Student Btn.
-function addStudent(student) {
-  if (sN.value != "" && sG.value != "" && sC.value != "") {
-    console.log("Boom!");
+// Creating a Position class
+class Position {
+  constructor(_title, _hours) {
+    this.title = _title;
+    this.hours = _hours;
+  }
+}
 
-    if (sS.value != "") {
-      console.log("Changing static value!");
-      Person.school = sS.value;
+// Creating an Employer class
+class Employer {
+  constructor(_name) {
+    this.name = _name;
+  }
+}
+
+// This function will fill the employer field in with the current value.
+function populateEmployerField(value) {
+  console.log("Setting populateEmployerField to: ", value);
+  employerInput.value = value;
+}
+
+
+// onClick event for Add Employee Btn.
+function addEmployee() {
+  if (eName.value != "" && eHours.value != "" &&
+    ePosition.value != "" && eParkingSpotNumberInput.value != "")
+  {
+    console.log("Values not empty. Allow creation of new Employee.");
+
+    // set the current employer objects name to the value given
+    currentEmployer.name = employerInput.value;
+
+    // Creating a variable that holds the value of the employees position converted to lowercase.
+    var titleGiven = ePosition.value.toLowerCase();
+    var personOrEmployee = null;
+
+    // If statement to check if the position title entered is equal to boss.
+    if(titleGiven === "boss") {
+      // If it equals boss a person is created with passed values and saved to my personOrEmployee variable.
+      personOrEmployee = new Person(eName.value, ePosition.value, eHours.value);
+    }
+    // Else for position check.
+    else {
+      // If the position does not equal boss a new position object is created.
+      var position = new Position(ePosition.value, eHours.value);
+      console.log("Created a new Position class instance: ", position);
+
+      // A new employee is created with passed values and saved to my personOrEmployee variable.
+      personOrEmployee = new Employee(eName.value, position, eParkingSpotNumberInput.value);
+      console.log("Created a new Employee class instance: ", personOrEmployee);
     }
 
-    // Creating instance of Person Class.
-    var student = new Person(sN, sG, sC);
-    student.name = sN.value;
-    student.grades = sG.value;
-    student.computer = sC.value;
+    console.log("Person or Employee object: ", personOrEmployee);
 
-    // Pushing new student to students array.
-    students.push(student);
+    // Pushing new Employee to employees array.
+    employees.push(personOrEmployee);
 
-    console.log(student);
-    console.log(students);
-    console.log(students.length);
+    console.log("Total employees: ", employees.length);
+    console.log("New array of employees: ", employees);
 
-    displayInfo()
-    clearInputs()
+    displayInfo();
+    clearInputs();
 
-    sN.focus();
-
-    return student;
+    eName.focus();
   }
 }
 
 // Creating function that takes in an input and resets it to null or default
 function clearInputs() {
-  sS.value = Person.school;
-  sN.value = '';
-  sG.value = '';
-  sC.value = '';
+  employerInput.value = currentEmployer.name;
+  eName.value = '';
+  eHours.value = '';
+  ePosition.value = '';
+  eParkingSpotNumberInput.value = '';
 }
 
-// Creating function to print Students to the table when Done Btn is pressed
-function showTable() {
-  console.log("Will Show Table!");
+// Creating function to print employees to the table when Calculate Totals Btn is pressed.
+function calculateTotals() {
+  var totalContainer = document.getElementById("totalContainer");
+  // Creating a new div.
+  var totalText = "<div>";
+
+  var hourTotal = 0;
+  var currentPersonOrEmployee;
+
+  // For loop to loop through employees array.
+  for( var i = 0; i < employees.length; i++ ) {
+    currentPersonOrEmployee = employees[i];
+
+    /* Calling my Utils class convertToIntArray function and passing it each
+    employee. Then saving the value to my hourInts variable. */
+    hourInts = Utils.convertToIntArray(currentPersonOrEmployee);
+
+    /* Calling my Utils class getTotalHours function and passing it my hours as
+    ints. Then adding the value to my hourTotal variable. */
+    hourTotal += Utils.getTotalHours(hourInts);
+  }
+
+  // Adding the new div to my totalContainer and populating it with a string and the total employee hours.
+  totalContainer.innerHTML = totalText + "Total Employee Hours (This Week): " + hourTotal;
 }
 
+// Creating displayInfo function to print all employee details to my table.
 function displayInfo() {
-  //var grades = [Utils.convertToIntArray(gradesField.value)];
-  var grades = gradesField.value;
-  var gradeInts = Utils.convertToIntArray(grades);
+  var hourInts;
+  var tableBody = document.getElementById("dataBody");
 
-  var table = document.getElementById("dataTable");
-  var row = document.createElement("tr");
-  var cell = document.createElement("td");
-  var cellText = document.createTextNode(document.getElementById("nameField").value);
-  cell.appendChild(cellText);
-  row.appendChild(cell);
+  // Reseting and emptying table to get rid of any existing values.
+  tableBody.innerHTML = "";
 
-  var cell = document.createElement("td");
-  var cellText = document.createTextNode(document.getElementById("gradesField").value);
-  cell.appendChild(cellText);
-  row.appendChild(cell);
+  var currentPersonOrEmployee;
 
-  // This shoud show average grades.
-  var cell = document.createElement("td");
-  //var cellText = document.createTextNode(Utils.getAverage(Utils.convertToIntArray(grades)));
-  //var cellText = document.createTextNode(Utils.getAverage(grades));
-  var cellText = document.createTextNode(Utils.getAverage(gradeInts));
-  cell.appendChild(cellText);
-  row.appendChild(cell);
+  // Looping through employees array and assigning each employee to the currentPersonOrEmployee variable
+  for( var i = 0; i < employees.length; i++ ) {
+    currentPersonOrEmployee = employees[i];
 
-  var cell = document.createElement("td");
-  var cellText = document.createTextNode(document.getElementById("computerField").value);
-  cell.appendChild(cellText);
-  row.appendChild(cell);
+    /* Calling my Utils class convertToIntArray function and passing it a
+    employee. Then setting the value to the hourInts variable */
+    hourInts = Utils.convertToIntArray(currentPersonOrEmployee);
 
-  // Show Static value.
-  var cell = document.createElement("td");
-  var cellText = document.createTextNode(Person.school);
-  cell.appendChild(cellText);
-  row.appendChild(cell);
+    // Creating a table row to hold the new data/cells
+    var row = document.createElement("tr");
 
-  table.appendChild(row);
+    // Creating a table data/cell that will display the employees name.
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(currentPersonOrEmployee.name);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    /* Creating a table data/cell that will display the hours string for a person("Whenever I want!") or a
+    employer which will display the hours for the week. */
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(currentPersonOrEmployee.getHoursString());
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    // Creating a table data/cell and if an employee is being created then this will display the average hours for the week.
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(Utils.getAverage(hourInts));
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    /* Creating a table data/cell that will display the title string for a person("Get back to work!";)
+    or a employee which will show position title*/
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(currentPersonOrEmployee.getTitleString());
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    // Creating a table data/cell that will display employer name.
+    var cell = document.createElement("td");
+    var cellText = document.createTextNode(currentEmployer.name);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    // Appending the new row to my table.
+    tableBody.appendChild(row);
+  }
 };
+
+// Variables
+var employerInput = document.getElementById('employerField');
+var eName = document.getElementById('employeeName');
+var eHours = document.getElementById('employeeHours');
+var ePosition = document.getElementById('employeePosition');
+var eParkingSpotNumberInput = document.getElementById("parkingSpotNumber");
+
+// Instantiating an instance of my Employer Object
+var currentEmployer = new Employer();
+
+// Creating an array to hold my Employees
+var employees = new Array;
+
+/* Created an Init function that sets the Employer to IBM and populates
+the input field with the value. */
+function init() {
+  console.log("Initializing page...");
+  currentEmployer.name = "IBM";
+  populateEmployerField(currentEmployer.name);
+}
+// Running my Init function on window load.
+window.onload = init();
--- src/js/utils.js
+++ src/js/utils.js
@@ -2,20 +2,59 @@ class Utils {
   constructor() {
     console.log('Utils created');
   }
+
+  // Method to calculate the total average of an array of numbers.
   static getAverage(arr) {
     var total = 0;
+
+    /* If the array is empty, which means the position title entered was boss, then
+    this string will be returned instead of the average hours. */
+    if(!arr.length) return "I Make my own hours!";
+
+    // Looping through each number in the array.
     arr.forEach(function(e){
       total += e;
-    })
+    });
+
+    // Dividing the total by the length of the passed array.
     return total / arr.length;
   }
 
-  static convertToIntArray(str) {
-    var temp = str.split(' ');
-    temp.forEach((el, index) => {
-      el = Number(el);
-      temp[index] = el;
+  // Method that takes in an array of numbers to calculate all employee hours.
+  static getTotalHours(arr) {
+    var total = 0;
+
+    /* If the array is empty, which means the position title entered was boss, then
+    0 is returned instead of the total hours. */
+    if(!arr.length) return 0;
+
+    // Looping through each number in the array.
+    arr.forEach(function(e){
+      total += e;
     });
-    return temp;
+
+    return total;
+  }
+
+  /* Method that takes in a created person or employee and converts the hours array
+  to a numbers array. */
+  static convertToIntArray(personOrEmployee) {
+    // Checks weather personOrEmployee is an instance of my Employee class and not a Person.
+    if(personOrEmployee instanceof Employee) {
+      // Getting the hours from the employee and saving it to my hoursString variable.
+      var hoursString = personOrEmployee.position.hours;
+      // Spliting the string up by spaces.
+      var temp = hoursString.split(' ');
+
+      // Looping through each of the parts of the split string and replacing it with a number.
+      temp.forEach((el, index) => {
+        el = Number(el);
+        temp[index] = el;
+      });
+      return temp;
+    }
+    else {
+      return [];
+    }
   }
 }
